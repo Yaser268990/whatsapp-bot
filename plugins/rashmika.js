0f9b69fda5c0d846c4bd3a3cd6ca262e56e62290
Yaser268990 <101202899+Yaser268990@users.noreply.github.com>
2022-05-05 12:37:16 +0300

    Update rashmika.js

--- plugins/rashmika.js
+++ plugins/rashmika.js
@@ -842,7 +842,7 @@ r_text[200] = "https://i.ibb.co/qjySbDs/rashmika-mandanna-20200304-0051.jpg";
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made By Bixby Mowl'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made By YASER BOT'})
 
     }));
 
