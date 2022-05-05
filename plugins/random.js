1db304dede179a6d24a50b4d7f979be7b25e25ff
Yaser268990 <101202899+Yaser268990@users.noreply.github.com>
2022-05-05 12:32:19 +0300

    Update random.js

--- plugins/random.js
+++ plugins/random.js
@@ -2,8 +2,7 @@ const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
-const NEED_WORD = "*يجب إدخال بعض الكلمات
-*"
+const NEED_WORD = "*يجب إدخال بعض الكلمات*"
 
 
 Asena.addCommand({ pattern: 'random ?(.*)', fromMe: false, desc: 'word image' }, (async (message, match) => {
