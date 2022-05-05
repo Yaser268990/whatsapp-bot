e3666dfc269b3986c89b957be08512c0a599b151
Yaser268990 <101202899+Yaser268990@users.noreply.github.com>
2022-05-05 12:27:34 +0300

    Update pdf.js

--- plugins/pdf.js
+++ plugins/pdf.js
@@ -9,17 +9,17 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const need = "*add link after command💌*"
+const need = "*أضف رابطًا بعد الأمر💌*"
 
 if (Config.WORKTYPE == 'private') {
 
-  Bixby.addCommand({ pattern: 'pdf ?(.*)', fromMe: true, desc: 'Get screenshot of links in pdf'}, (async (message, match) => {
+  Bixby.addCommand({ pattern: 'pdf ?(.*)', fromMe: true, desc: 'احصل على لقطة شاشة للروابط في ملف pdf'}, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(need);
 
     var rashi = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
     
-    await message.sendMessage(Buffer.from(rashi.data), MessageType.document, { filename: 'Bixby.pdf', mimetype: Mimetype.pdf});
+    await message.sendMessage(Buffer.from(rashi.data), MessageType.document, { filename: 'YASER BOT.pdf', mimetype: Mimetype.pdf});
 
   }));
 }
