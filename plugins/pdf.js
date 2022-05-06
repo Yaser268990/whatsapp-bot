7e2fb94f9a910a9af012a59b725ac2f7ffc78d25
Yaser268990 <101202899+Yaser268990@users.noreply.github.com>
2022-05-06 03:05:56 +0300

    Update pdf.js

--- plugins/pdf.js
+++ plugins/pdf.js
@@ -26,13 +26,13 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-  Bixby.addCommand({ pattern: 'pdf ?(.*)', fromMe: false, desc: 'Get screenshot of links in pdf'}, (async (message, match) => {
+  Bixby.addCommand({ pattern: 'pdf ?(.*)', fromMe: false, desc: 'احصل على لقطة شاشة للروابط في ملف pdf'}, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(need);
 
     var rashi = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer.from(rashi.data), MessageType.document, {quoted: message.data, filename: 'Bixby.pdf', mimetype: Mimetype.pdf});
+    await message.sendMessage(Buffer.from(rashi.data), MessageType.document, {quoted: message.data, filename: 'YASER BOT.pdf', mimetype: Mimetype.pdf});
 
   }));
 
